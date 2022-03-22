e72685a1d7af46293c916e1d6b208f4685451346
manthankumbhar <kum.manthan@gmail.com>
2022-03-22 15:37:11 +0530

    final changes

--- src/Components/SlateEditor/SlateEditor.js
+++ src/Components/SlateEditor/SlateEditor.js
@@ -1,4 +1,10 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, {
+  createRef,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import "Components/SlateEditor/SlateEditor.scss";
 import {
   createEditor,
@@ -6,7 +12,9 @@ import {
   Transforms,
   Element as SlateElement,
 } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
+import { Slate, Editable, withReact, ReactEditor } from "slate-react";
+import Menu, { Item as MenuItem } from "rc-menu";
+import "rc-menu/assets/index.css";
 
 export default function SlateEditor() {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -19,6 +27,40 @@ export default function SlateEditor() {
     ]
   );
   const LIST_TYPES = ["numbered-list", "bulleted-list"];
+  const [showMenu, setShowMenu] = useState(false);
+  const [coordinates, setCoordinates] = useState("");
+  const allowedTags = [
+    {
+      label: "h1",
+      value: "heading-one",
+    },
+    {
+      label: "h2",
+      value: "heading-two",
+    },
+    {
+      label: "quote",
+      value: "block-quote",
+    },
+    {
+      label: "numbered",
+      value: "numbered-list",
+    },
+    {
+      label: "bullets",
+      value: "bulleted-list",
+    },
+    {
+      label: "p",
+      value: "paragraph",
+    },
+  ];
+  const [menuOptions, setMenuOptions] = useState(allowedTags);
+  const menuFocus = createRef();
+
+  useEffect(() => {
+    menuFocus.current?.focus();
+  }, [menuFocus]);
 
   const renderElement = useCallback((props) => {
     switch (props.element.type) {
@@ -124,6 +166,70 @@ export default function SlateEditor() {
     },
   };
 
+  const getCoordinates = () => {
+    let x, y;
+    const selection = window.getSelection();
+    if (selection.rangeCount !== 0) {
+      const range = selection.getRangeAt(0).cloneRange();
+      range.collapse(false);
+      const rect = range.getClientRects()[0];
+      if (rect) {
+        x = rect.top;
+        y = rect.left;
+      }
+    }
+    return { top: x > 120 ? x - 150 : x + 20, left: y };
+  };
+
+  const closeMenu = useCallback(() => {
+    setShowMenu(false);
+    document.removeEventListener("click", closeMenu);
+  }, []);
+
+  const openMenu = useCallback(() => {
+    const position = getCoordinates();
+    setCoordinates(position);
+    setShowMenu(true);
+    document.addEventListener("click", closeMenu);
+  }, [closeMenu]);
+
+  const renderMenu = () => {
+    return (
+      <Menu
+        className="editor__menu"
+        style={coordinates}
+        ref={menuFocus}
+        onKeyDown={(event) => {
+          if (event.key === " " || event.key === "Backspace") {
+            closeMenu();
+            ReactEditor.focus(editor);
+            event.preventDefault();
+          }
+
+          if (event.key === "Enter") {
+            event.preventDefault();
+          }
+        }}
+      >
+        {menuOptions.map((item, key) => {
+          return (
+            <MenuItem
+              tabIndex="0"
+              key={key}
+              onClick={() => {
+                CustomEditor.toggleBlock(editor, item.value);
+                closeMenu();
+                ReactEditor.focus(editor);
+              }}
+            >
+              {item.label}
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    );
+  };
+
   return (
     <div className="editor">
       <Slate
@@ -140,11 +246,32 @@ export default function SlateEditor() {
           }
         }}
       >
+        {showMenu ? renderMenu() : null}
         <Editable
+          id="editor__editable"
           className="editor__area"
           renderElement={renderElement}
           renderLeaf={renderLeaf}
           onKeyDown={(event) => {
+            if (event.key === "/") {
+              openMenu();
+            }
+            if (event.key === " " || event.key === "Backspace") {
+              closeMenu();
+            }
+            if (event.key === "Enter") {
+              event.preventDefault();
+              const isList = LIST_TYPES.includes(editor.getFragment()[0].type);
+              return isList
+                ? Transforms.insertNodes(editor, {
+                    type: "list-item",
+                    children: [{ text: "" }],
+                  })
+                : Transforms.insertNodes(editor, {
+                    type: "paragraph",
+                    children: [{ text: "" }],
+                  });
+            }
             if (!event.ctrlKey) {
               return;
             }
